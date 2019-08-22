bbfb58e3d06a6e4b126b3d81798bf7ad3c585a8d
Guilherme Hashioka <guilherme.hashioka@gmail.com>
2019-08-22 12:06:53 -0300

    Force number to string conversion
    - The problem
    It seems that there is a problem with type mismatch on Cosmic.
    The issue is that if you put a the number '10' in a textfield, then the response is converted to a `number`, but if you put the number '07', then it gets converted to a `string`.
    
    - Workaround
    convert every number to a string, which can remove this type mismatch issue

--- src/normalize.js
+++ src/normalize.js
@@ -11,5 +11,29 @@ exports.processObject = (type, item, createContentDigest) => {
       contentDigest: createContentDigest(item),
     },
   }
+  // convertNumberValuesToString(item)
   return Object.assign({}, item, nodeMetadata)
+}
+
+/**
+ *
+ * # The problem
+ * It seems that there is a problem with type mismatch on Cosmic.
+ * The issue is that if you put a the number '10' in a textfield, then the response is converted to a `number`, but if you put the number '07', then it gets converted to a `string`.
+ *
+ * # Workaround
+ * This method converts every number to a string, which can remove this type mismatch issue.
+ *
+ */
+const convertNumberValuesToString = obj => {
+  if (!obj) {
+    return
+  }
+  Object.keys(obj).map(property => {
+    if (typeof obj[property] == 'number') {
+      obj[property] = String(obj[property])
+    } else if (typeof obj[property] == 'object') {
+      convertNumberValuesToString(obj[property])
+    }
+  })
 }
