b9dd12e76bf70c6dcc22997dfe078d0f35819a8e
Guilherme Hashioka <guilherme.hashioka@gmail.com>
2019-08-22 14:47:36 -0300

    Avoid type mismatch warning
     - The problem
     It seems that there is a problem with type mismatch on Cosmic.
     The issue is that if you put a the number '10' in a textfield, then the response is converted to a `number`, but if you put the number '07', then it gets converted to a `string`.
    
     - Workaround
     This method converts every number to a string, which can remove this type mismatch issue.

--- src/nodes.js
+++ src/nodes.js
@@ -15,6 +15,30 @@ const { createNodeFactory } = createNodeHelpers({
 export const Node = (type, node) =>
   createNodeFactory(type, node => {
     node.id = node._id
+    convertNumberValuesToString(node)
     delete node._id
     return node
   })(node)
+
+/**
+ *
+ * # The problem
+ * It seems that there is a problem with type mismatch on Cosmic.
+ * The issue is that if you put a the number '10' in a textfield, then the response is converted to a `number`, but if you put the number '07', then it gets converted to a `string`.
+ *
+ * # Workaround
+ * This method converts every number to a string, which can remove this type mismatch issue.
+ *
+ */
+const convertNumberValuesToString = obj => {
+  if (!obj) {
+    return
+  }
+  Object.keys(obj).map(property => {
+    if (typeof obj[property] == 'number') {
+      obj[property] = String(obj[property])
+    } else if (typeof obj[property] == 'object') {
+      convertNumberValuesToString(obj[property])
+    }
+  })
+}
