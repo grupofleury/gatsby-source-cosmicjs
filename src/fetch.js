b510842b56df1b9516acf4c76137cb394a867163
Guilherme Hashioka <guilherme.hashioka@gmail.com>
2019-10-05 22:11:34 -0300

    feat: 🎸 fetch more than 1000 objects
    https://github.com/cosmicjs/gatsby-source-cosmicjs/pull/4/files

--- src/fetch.js
+++ src/fetch.js
@@ -1,39 +1,69 @@
-const axios = require('axios')
-const queryString = require('query-string')
-const _ = require('lodash')
+const axios = require('axios');
+const { isObject, startsWith, forEach, concat } = require('lodash');
 
-module.exports = async ({
-  apiURL,
-  bucketSlug,
-  objectType,
-  apiAccess,
-  preview,
-}) => {
-  console.time('Fetch Cosmic JS data')
+module.exports = async ({ apiURL, bucketSlug, objectType, apiAccess }) => {
+  const timeLabel = `Fetch Cosmic JS data for (${objectType})`
+  console.time(timeLabel)
   console.log(`Starting to fetch data from Cosmic JS (${objectType})`)
-
-  // Define URL params
-  let urlParams = queryString.stringify({
-    type: objectType,
-    ...(preview && { status: 'all' }),
-    ...(apiAccess && apiAccess.read_key && { read_key: apiAccess.read_key }),
-  })
-
+  let objects = []
+  const limit = 1000
+  let skip = 0
   // Define API endpoint.
-  let apiEndpoint = `${apiURL}/${bucketSlug}/objects?${urlParams}`
+  let apiEndpoint = `${apiURL}/${bucketSlug}/objects?type=${objectType}`
 
-  // Make API request.
+  if (apiAccess.hasOwnProperty('read_key') && apiAccess.read_key.length !== 0) {
+    apiEndpoint = apiEndpoint + `&read_key=${apiAccess.read_key}`
+    apiEndpoint = apiEndpoint + `&limit=${limit}`
+  }
+
+  // Make initial API request.
   const documents = await axios(apiEndpoint)
 
-  // Query all data from endpoint
-  console.timeEnd('Fetch Cosmic JS data')
+  // Check for empty object type
+  if (documents.data.objects === undefined) {
+    console.error(`${objectType} error: ${documents.message}`)
+    console.timeEnd(timeLabel)
+    return objects
+  }
+
+  if (documents.data.objects) {
+    objects = documents.data.objects
+  }
+
+  // check if there's more that request limit of objects for object type
+  if (documents.data.total && documents.data.total > limit) {
+    // Query all data from endpoint
+    // calculate number of calls to retrieve entire object type
+    const additionalCallsRequired = Math.ceil(documents.data.total / limit) - 1
+
+    for (let i = 0; i < additionalCallsRequired; i += 1) {
+      // skip previously requested objects
+      skip = skip + limit
+      let skipEndpoint = apiEndpoint + `&skip=${skip}`
+      // Query next batch from endpoint
+      let response = await axios(skipEndpoint)
+      if (response.data.objects) {
+        objects = concat(objects, response.data.objects)
+      } else {
+        console.error(`${objectType} fetch issue: ${documents.message}`)
+        break
+      }
+    }
+  }
+
+  console.log(
+    `Fetched ${objects.length} ${
+      objects.length === 1 ? 'object' : 'objects'
+    } for object type: ${objectType}`
+  )
+  console.timeEnd(timeLabel)
 
   // Map and clean data.
-  if (documents.data.objects) {
-    return documents.data.objects.map(item => clean(item))
-  } else {
-    return []
+  if (objects.length > 0) {
+    objects = objects.map(item => clean(item))
   }
+
+  return objects
 }
 
 /**
@@ -43,10 +73,10 @@ module.exports = async ({
  * @returns {object} output - Object cleaned
  */
 const clean = item => {
-  _.forEach(item, (value, key) => {
-    if (_.startsWith(key, `__`)) {
+  forEach(item, (value, key) => {
+    if (startsWith(key, `__`)) {
       delete item[key]
-    } else if (_.isObject(value)) {
+    } else if (isObject(value)) {
       item[key] = clean(value)
     }
   })
